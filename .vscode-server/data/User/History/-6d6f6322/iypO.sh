cc1902b4833a94ca9921c7ef7378b9fbf081432c
git <1904193125@qq.com>
2023-12-20 00:33:43 +0000

    delete

--- .vscode-server/data/User/History/-6d6f6322/iypO.sh
+++
@@ -1,6 +0,0 @@
-apt-get update
-apt-get install pip
-pip install pillow
-pip install min_dalle
-
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
--- .vscode-server/data/User/History/-6d6f6322/sgjF.sh
+++
@@ -1,3 +0,0 @@
-apt-get update
-apt-get install pip
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
